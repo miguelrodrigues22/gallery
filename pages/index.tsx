6ff67802d749e785ea0b5b6bb2e9ffbb892cf260
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2022-09-01 16:26:55 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.SUPABASE_SERVICE_ROLE_KEY || '',
-);
+import { useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
 
 export async function getStaticProps() {
-  const { data } = await supabaseAdmin.from('images').select('*');
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  )
+
+  const { data } = await supabaseAdmin.from('images').select('*').order('id')
   return {
     props: {
-      images: data
-    }
-  };
+      images: data,
+    },
+  }
 }
 
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 type Image = {
   id: number
@@ -27,39 +28,34 @@ type Image = {
   username: string
 }
 
-
-
-function cn(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
-}
-
 export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {images.map((image) => (
           <BlurImage key={image.id} image={image} />
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 function BlurImage({ image }: { image: Image }) {
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true)
+
   return (
     <a href={image.href} className="group">
-      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
           src={image.imageSrc}
           layout="fill"
           objectFit="cover"
           className={cn(
-            'group-hover:opacity-75 duration-700 ease-in-out',
+            'duration-700 ease-in-out group-hover:opacity-75',
             isLoading
-              ? 'grayscale blur-2xl scale-110'
-              : 'grayscale-0 blur-0 scale-100'
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
           )}
           onLoadingComplete={() => setLoading(false)}
         />
@@ -67,5 +63,5 @@ function BlurImage({ image }: { image: Image }) {
       <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
     </a>
-  );
+  )
 }
